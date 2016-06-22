8021b46370c2bde7fca88383b2a1378af860ff35
zentarul <zentarul@gmail.com>
2016-06-21 23:09:57 -0400

    Added modular support for display options Added speed display WIP for angle to prograde display

--- ABookCaseOrbitalReferenceSystem/ABookCaseOrbitalReferenceSystem.cs
+++ ABookCaseOrbitalReferenceSystem/ABookCaseOrbitalReferenceSystem.cs
@@ -12,7 +12,26 @@ namespace ABCORS
         private bool _mouseOver = false;
         private PatchedConics.PatchCastHit _mousedOverPatch;
 
-        private Rect _popup = new Rect(0f, 0f, 160f, 88f);
+        private Rect _popup = new Rect(0f, 0f, 160f, 160f);
+
+        private bool _showTime = true;
+        private bool _showAltitude = true;
+        private bool _showSpeed = false;
+        private bool _showEjectionAngle = false;
+
+        protected void Start()
+        {
+            PluginConfiguration config = PluginConfiguration.CreateForType<ABookCaseOrbitalReferenceSystem>();
+            config.load();
+            float rectWidth = config.GetValue<int>("displayWidth", 160);
+            float rectHeight = config.GetValue<int>("displayHeight", 160);
+            _popup.Set(0, 0, rectWidth, rectHeight);
+            _showTime = config.GetValue<bool>("showTime", _showTime);
+            _showAltitude = config.GetValue<bool>("showAltitude", _showAltitude);
+            _showSpeed = config.GetValue<bool>("showSpeed", _showSpeed);
+            _showEjectionAngle = config.GetValue<bool>("showEjectionAngle", _showEjectionAngle);
+            config.save();
+        }
 
         private void Awake()
         {
@@ -42,10 +61,32 @@ namespace ABCORS
             Orbit orbit = _mousedOverPatch.pr.patch;
             Vector3d deltaPos = orbit.getPositionAtUT(_mousedOverPatch.UTatTA) - orbit.referenceBody.position;
             double altitude = deltaPos.magnitude - orbit.referenceBody.Radius;
+            double speed = orbit.getOrbitalSpeedAt(orbit.getObtAtUT(_mousedOverPatch.UTatTA));
+
+            string labelText = "";
+            if (_showTime)
+            {
+                labelText += "T: " + KSPUtil.PrintTime((int)(Planetarium.GetUniversalTime() - _mousedOverPatch.UTatTA), 5, true) + "\n";
+            }
+            if (_showAltitude)
+            {
+                labelText += "Alt: " + altitude.ToString("N0", CultureInfo.CurrentCulture) + "m\n";
+            }
+            if (_showSpeed)
+            {
+                labelText += "Vel: " + speed.ToString("N0", CultureInfo.CurrentCulture) + "m/s\n";
+            }
+            if (_showEjectionAngle && orbit.referenceBody.orbit != null)
+            {
+                Vector3d bodyFwd = orbit.referenceBody.orbit.getOrbitalVelocityAtUT(_mousedOverPatch.UTatTA);
+                Vector3d shipNormal = deltaPos;
+                double angle = Vector3d.Angle(shipNormal, bodyFwd);
+                labelText += "E\u03B1: " + angle.ToString("N2", CultureInfo.CurrentCulture) + "\u00B0\n";
+                labelText += bodyFwd.ToString() + "\n" + shipNormal.ToString();
+            }
 
             GUILayout.BeginArea(GUIUtility.ScreenToGUIRect(_popup));
-            GUILayout.Label("T: " + KSPUtil.PrintTime((int)(Planetarium.GetUniversalTime() - _mousedOverPatch.UTatTA), 5, true) + "\nAlt: " + altitude.ToString("N0", CultureInfo.CurrentCulture) + "m");
-
+            GUILayout.Label(labelText);
             GUILayout.EndArea();
         }
 
